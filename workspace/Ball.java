9e079e1edf41b8973a79c279750deeee071a27b5
DefaultUsernam <121245757+DefaultUsernam@users.noreply.github.com>
2024-12-06 19:03:46 +0000

    I programmed Ball and Paddle with comments 12/6/2024

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,75 @@
+//
+//  Class author:  Ulises Cantor
+//  Date created:  12/4/2024
+//  General description: Creates a ball object that moves bounces off of the walls, bricks, and paddle on the program. 
+//
+
+
 import java.awt.*;
 
 public class Ball {
 	//your code here!
-	
+
+
 	//don't forget you need instance variables:
-	
+    private int y;
+    private int x;
+    private int radius;
+    private int yVelocity;
+    private int xVelocity;
+
 	
 	//constructor(s):
-	
+	public Ball(int nx, int ny, int r){
+		this.x = nx;
+		this.y = ny;
+		this.radius = r;
+	}
 	//methods:
-
+	
+    public int getYpos(){
+		return y;
+	}
+    public int getXpos(){
+		return x;
+	}
+    public int getSize(){
+		return radius;
+	}
+	//  Pre-condition: yVelocity is not 0
+	//  Post-condition: Reverses velocity of the ball's y direction by multiplying the velocity by -1
+    public void reverseY(){
+		yVelocity *= -1;
+	}
+    public void setXVelocity(int veloX){
+		xVelocity = veloX;
+	}
+	//  Pre-condition: xVelocity is not 0
+	//  Post-condition: Reverses velocity of the ball's x direction, changes the sign from + to - or vise versa
+    public void reverseX(){
+		xVelocity *= -1;
+	}
+    public void setX(int newerX){
+		x = newerX;
+	}
+	//  Pre-condition: Technically, the ball x and y shouldn't be off screen (unless the player loses) or inside the confines of another object, but it doesn't matter it will run regardless. 
+	//  If the velocities of either x or y coordinates are 0, the ball won't move in that respective direction at all
+	//  Post-condition: x and y coordinate change by their respective velocity amounts
+    public void move(){
+		x += xVelocity;
+		y += yVelocity;
+	}
+    public void setY(int newerY){
+		y = newerY;
+	}
+    public void setYVelocity(int veloY){
+		yVelocity = veloY;
+	}
+	//  Pre-condition: Valid graphics input
+	//  Post-condition: Applies a color to the ball
+	public void draw(Graphics g){
+		g.fillOval(x, y, radius, radius);
+	}
+	
+	
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,81 @@
+//
+//  Class author:  Ulises Cantor
+//  Date created:  12/4/2024
+//  General description: Creates a paddle object that represents the player, which reflects the ball sprite upon collision and can be moved left and right.
+//
+
+
 import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
 
+	
+
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
-	
+	private int width;
+    private int height;
+    private int x;
+    private int y;
+    private int velocity;
+
 	//constructor(s):
-	
+	public Paddle(int nx, int ny, int w, int h) {
+		this.width = w;
+		this.height = h;
+		this.x = nx;
+		this.y = ny;
+	}
+
 	//methods:
+
+    public int getY(){
+		return y;
+	}
+    public int getX(){
+		return x;
+	}
+    public int getWidth(){
+		return width;
+	}
+    public int getHeight(){
+		return height;
+	}
+    public int getVelocity(){
+		return velocity;
+	}
+	//  Pre-condition: Will run no matter what, just won't do anything if velocity is 0.
+	//  Post-condition: Shifts the x coordinate of the paddle in intervals of the velocity amount
+    public void move(){
+		x += velocity;
+	}
+	//  Pre-condition: Not null (or 0 because that wouldn't change anything)
+	//  Post-condition: Changes the velocity amount by the input integer
+    public void addVelocity(int plusVelo){
+	
+		velocity += plusVelo;
+		if (velocity>20){
+			velocity = 20;
+		}
+		else if (velocity < -20){
+			velocity = -20;
+		}
+
+	}
+	public void setX(int newestX){
+		x = newestX;
+	}
+	public void setVelocity(int newestVelo){
+		velocity = newestVelo;
+	}
+	//  Pre-condition: Valid graphics input
+	//  Post-condition: Applies a color to the ball
+    public void draw(Graphics g){
+		g.fillRect(x, y, width, height);
+	}
+
 	
 	
 }
